a6801a800f669c197a6deba7a36b9e346687be76
@Lqaxxor <88044293+alqaxxor@users.noreply.github.com>
2022-09-16 23:33:42 +0500

    crudapp

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const path=require('path')
 require('dotenv').config()
 const app=express()
 const cookieParser = require('cookie-parser')
+const PORT = process.env.PORT || 5050
 
 const ejs=require('ejs')
+
 const router = require('./routers/router')
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(cookieParser())
 app.use(express.json())
 
+
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
 
 app.use(router)
 
-app.listen(5050,console.log(5050))
+app.listen(PORT, () => console.log(`${PORT}`))
